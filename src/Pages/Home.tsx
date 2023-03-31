36d0a8ddd5be24453a59067f4b36f45e68c83160
ghassenbhl <ghassen.bouhlel@gmail.com>
2023-03-31 11:51:04 +0200

    Footer

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "@/Component/Button";
 import { Input } from "@chakra-ui/react";
+import Footer from "@/Component/Footer";
 type Props = {};
 
 const Home = ({}: Props) => {
   return (
     <div>
       <Button variant="solid">Button</Button>
+      <Footer/>
     </div>
   );
 };
