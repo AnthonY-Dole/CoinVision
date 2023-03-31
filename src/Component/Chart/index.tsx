7fad9570c80f216f277dc4bf82bb27dbb9e90b10
Anthony Dole <anthony.dole.pro@mail.com>
2023-03-31 15:49:56 +0200

    add chart

--- src/Component/Chart/index.tsx
+++ src/Component/Chart/index.tsx
@@ -1,6 +1,6 @@
 import {
-  LineChart,
-  Line,
+  AreaChart,
+  Area,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -8,19 +8,57 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type ChartProps = {
   data: any;
 };
 
-const Chart = ({ data }: ChartProps) => {
+const Chart = (props: ChartProps) => {
+  const { data } = props;
+
+  const [chart, setChart] = useState({});
+
+  useEffect(() => {
+    const chartObjects = data?.chart?.map((data: any) => {
+      return {
+        timestamp: data[0],
+        prix: data[1],
+        value2: data[2],
+        value3: data[3],
+      };
+    });
+    setChart(chartObjects);
+  }, [data]);
+
+  console.log(chart);
+
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <LineChart width={500} height={300} data={data}>
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
-      </LineChart>
+      <AreaChart
+        width={730}
+        height={250}
+        data={chart}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="10%" stopColor="#425de6" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="5 3" />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="prix"
+          stroke="#8884d8"
+          fillOpacity={1}
+          fill="url(#colorUv)"
+        />
+      </AreaChart>
     </ResponsiveContainer>
   );
 };
